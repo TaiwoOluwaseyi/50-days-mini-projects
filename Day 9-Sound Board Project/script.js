05f3481e749e7ef133a3a277924fb56210e92bfd
Mabel Taiwo <seyitaiwo21@gmail.com>
2022-04-06 13:07:17 +0100

    another commit

--- Day 9-Sound Board Project/script.js
+++ Day 9-Sound Board Project/script.js
@@ -5,4 +5,22 @@ sounds.forEach(sound => {
   btn.classList.add('btn')
 
   btn.innerText = sound
+
+  btn.addEventListener('click', () => {
+    stopSongs()
+
+    document.getElementById(sound).play()
+  })
+
+  document.getElementById('buttons').appendChild(btn)
 })
+
+function stopSongs () {
+  sounds.forEach(sound => {
+    const song = document.getElementById
+    (sound)
+
+    song.pause()
+    song.currentTime = 0;
+  })
+}
