9ea8fb3e24f5712213f167beec4648f9b5cdaced
Mabel Taiwo <seyitaiwo21@gmail.com>
2022-04-11 15:26:32 +0100

    another commit

--- Day 12-Movie App/script.js
+++ Day 12-Movie App/script.js
@@ -1,49 +1,67 @@
 const API_URL =
-  'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6eab65988232881a1da575bf0f2f8b10&page=1'
-  const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-  const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=6eab65988232881a1da575bf0f2f8b10&query="'
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6eab65988232881a1da575bf0f2f8b10&page=1";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API =
+  'https://api.themoviedb.org/3/search/movie?api_key=6eab65988232881a1da575bf0f2f8b10&query="';
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
 
-const form = document.getElementById('form')
-const search = document.getElementById('search')
+// Get Initial movies
+getMovies(API_URL);
 
-  // Get Initial movies 
-  getMovies(API_URL)
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
 
-  async function getMovies(url) {
-    const res = await fetch (url)
-    const data = await res.json()
+  showMovies(data.results);
+}
 
-    console.log(data.results)
+function showMovies(movies) {
+  main.innerHTML = "";
 
-  }
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
 
-  form.addEventListener('submit', (e) => {
-    e.preventDefault()
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
 
-    const searchTerm = search.value
+    movieEl.innerHTML = `
+      <img src="${IMG_PATH + poster_path}" alt="${title}">
+      <div class="movie-info">
+        <h3>${title}</h3>
+        <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+     </div>
 
-    if(searchTerm && searchTerm !== '') {
-      getMovies(SEARCH_API + searchTerm)
-
-      search.value = ''
-    } else {
-        window.location.reload()
-    }
+      <div class="overview">
+        <h3>Overview</h3>
+       ${overview}
+      </div>
+      `
+      main.appendChild(movieEl)
   })
+}
 
+function getClassByRate(vote) {
+  if(vote >= 8) {
+    return 'green'
+  } else if(vote >= 5) {
+    return 'orange'
+  }else {
+    return 'red'
+  }
+}
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
+  const searchTerm = search.value;
 
+  if (searchTerm && searchTerm !== "") {
+    getMovies(SEARCH_API + searchTerm);
 
-
-
-
-
-
-
-
-
-
-
-
-  
+    search.value = "";
+  } else {
+    window.location.reload();
+  }
+});
