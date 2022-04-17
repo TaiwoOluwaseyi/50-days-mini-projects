8b45c38dcb235f27bc1bccd86885dd5ef0636ae1
Mabel Taiwo <seyitaiwo21@gmail.com>
2022-04-17 18:44:02 +0100

    another commit

--- Day 14-Theme Clock/script.js
+++ Day 14-Theme Clock/script.js
@@ -1,9 +1,10 @@
-const hourEl = document.querySelector(".hour");
-const minuteEl = document.querySelector(".minute");
-const secondEl = document.querySelector(".second");
-const timeEl = document.querySelector(".time");
-const dateEl = document.querySelector(".date");
-const toggleEl = document.querySelector(".toggle");
+const hourEl = document.querySelector('.hour')
+const minuteEl = document.querySelector('.minute')
+const secondEl = document.querySelector('.second')
+const timeEl = document.querySelector('.time')
+const dateEl = document.querySelector('.date')
+const toggleEl = document.querySelector('.toggle')
+
 
 const days = [
   "Sunday",
@@ -31,38 +32,30 @@ const months = [
 
 toggleEl.addEventListener('click', (e) => {
   const html = document.querySelector('html')
-  if(html.classList.contains('dark')) {
-    html.classList.remove('dark')
+  if (html.classList.contains('dark')) {
+    html.classList.remove('dark');
     e.target.innerHTML = 'Dark mode'
   } else {
-      html.classList.add("dark");
-      e.target.innerHTML = "Light mode";
+    html.classList.add('dark');
+    e.target.innerHTML = 'Light mode'
   }
 })
 
-
-function setTime(){
-  const time = newDate()
+function setTime()  {
+  const time = new Date();
   const month = time.getMonth()
   const day = time.getDay()
-  const hours = time.getHours
+  const hours = time.getHours()
   const hoursForClock = hours % 12
   const minutes = time.getMinutes()
   const seconds = time.getSeconds()
 
-  hourEl.style.tranform = `translate(-50%, -100%) rotate(30deg)`;
+  hourEl.style.transform = `translate(-50%, -100%)  rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out-min;
 }
 
 setTime()
-
-
-
-
-
-
-
-
-
-
-
 
