35c531f4160c6dd4450c3590b680b2e6d3b84af3
Mabel Taiwo <seyitaiwo21@gmail.com>
2022-04-17 20:44:56 +0100

    another commit

--- Day 15-Random Choice Picker/script.js
+++ Day 15-Random Choice Picker/script.js
@@ -5,6 +5,15 @@ textarea.focus()
 
 textarea.addEventListener('keyup', (e) => {
   createTags(e.target.value)
+
+  if(e.key === 'Enter') {
+    setTimeout(() => {
+        e.target.value = ''
+    }, 10)
+    
+
+    randomSelect()
+  }
 })
 
 function createTags(input) {
@@ -19,3 +28,49 @@ function createTags(input) {
     tagsEl.appendChild(tagEl)
   })
 }
+
+function randomSelect() {
+  const times = 30 
+
+  const interval = setInterval(()=>{
+      const randomTag = pickRandomTag()
+
+      highlightTag(randomTag)
+
+      setTimeout(() => {
+          unHighlightTag(randomTag)
+      }, 100)
+  }, 100);
+
+  setTimeout(() => {
+     clearInterval(interval)
+
+    setTimeout(() => {
+      const random = pickRandomTag()
+
+      highlightTag(randomTag)
+    }, 100)
+  }, times * 100)
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll('.tag')
+  return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag) {
+  tag.classList.add('highlight')
+}
+
+function unHighlightTag(tag) {
+  tag.classList.remove('highlight')
+}
+
+
+
+
+
+
+
+
+
