fbff3555b8993da829f0714481b987170d590272
Mabel Taiwo <seyitaiwo21@gmail.com>
2022-04-30 19:15:19 +0100

    another commit

--- Day 24-Github User Profile/script.js
+++ Day 24-Github User Profile/script.js
@@ -1,26 +1,57 @@
-const APIURL = 'https://api.github.com/users/'
+const APIURL = "https://api.github.com/users/";
 
-const form = document.getElementById('form')
-const search = document.getElementById('search')
+const main = document.getElementById('main')
+const form = document.getElementById("form");
+const search = document.getElementById("search");
 
 async function getUser(username) {
-   try {
-      const { data } = await axios(APIURL + username)
+  try {
+    const { data } = await axios(APIURL + username);
 
-      console.log(data)
-  } catch(err) {
-    console.log(err)
-  } 
+    createUserCard(data);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
+function createUserCard(user) {
+  const cardHTML = ` 
+  <div class="card">
+        <div>
+          <img
+            src="${user.avatar_url}"
+            alt="${user.name}"
+            class="avatar"
+          />
+        </div>
 
-  const user = search.value
+        <div class="user-info">
+          <h2>${user.name}</h2>
+          <p>${user.bio}></p>
 
-  if(user) {
-    getUser(user)
+          <ul>
+            <li>${user.followers} <strong>Followers</strong></li>
+            <li>${user.following}<strong>Following</strong></li>
+            <li>${user.public_repos}<strong>Repositories</strong></li>
+          </ul>
+          
+           <div id="repos"></div>
+        </div>
+      </div>
+          `
 
-    search.value = ''
+          main.innerHTML = cardHTML
+
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const user = search.value;
+
+  if (user) {
+    getUser(user);
+
+    search.value = "";
   }
-})
+});
