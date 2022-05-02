c4a34f6ab38ae4f6057b4e80e860e3dc3fbefc30
Mabel Taiwo <seyitaiwo21@gmail.com>
2022-05-02 11:42:30 +0100

    another commit

---
+++ Day 25-Vertical UI Slider/script.js
@@ -0,0 +1,11 @@
+const sliderContainer = document.querySelector('.slider-container')
+const slideRight = document.querySelector('.right-slide')
+const slideLeft = document.querySelector(".left-slide");
+const upButton = document.querySelector(".up-button");
+const downButton = document.querySelector(".down-button");
+const slidesLength = slideRight.querySelectorAll('div').length;
+
+let activeSlideIndex = 0
+
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
+
