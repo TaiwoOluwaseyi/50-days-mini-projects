7994cc8d6b75f5ef1b6e00afb8a88dea1762d286
Mabel Taiwo <seyitaiwo21@gmail.com>
2022-05-02 12:20:20 +0100

    another commit

--- Day 25-Vertical UI Slider/script.js
+++ Day 25-Vertical UI Slider/script.js
@@ -9,3 +9,41 @@ let activeSlideIndex = 0
 
 slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
 
+upButton.addEventListener('click', () => changeSlide('up'))
+downButton.addEventListener("click", () => changeSlide("down"));
+
+const changeSlide = (direction) => {
+  const sliderHeight = sliderContainer.clientHeight
+  if(direction === 'up') {
+    activeSlideIndex++
+    if(activeSlideIndex > slidesLength - 1) {
+      activeSlideIndex = 0
+    }
+  } else if(direction === 'down') {
+    activeSlideIndex--
+    if(activeSlideIndex < 0) {
+      activeSlideIndex = slidesLength - 1
+    }
+  } 
+
+  slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+   slideLeft.style.transform = `translateY(${
+     activeSlideIndex * sliderHeight
+   }px)`;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
