da21b8388e84cf2b54c38cc094606ac0665c0d91
Mabel Taiwo <seyitaiwo21@gmail.com>
2022-05-04 17:11:03 +0100

    another commit

--- Day 14-Theme Clock/script.js
+++ Day 14-Theme Clock/script.js
@@ -3,37 +3,37 @@ const minuteEl = document.querySelector('.minute')
 const secondEl = document.querySelector('.second')
 const timeEl = document.querySelector('.time')
 const dateEl = document.querySelector('.date')
-const toggleEl = document.querySelector('.toggle')
+const toggle = document.querySelector('.toggle')
 
 
 const days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+]
 const months = [
-  "Jan",
-  "Feb",
-  "March",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
-];
+  'Jan',
+  'Feb',
+  'March',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+]
 
-toggleEl.addEventListener('click', (e) => {
+toggle.addEventListener('click', (e) => {
   const html = document.querySelector('html')
   if (html.classList.contains('dark')) {
-    html.classList.remove('dark');
+    html.classList.remove('dark')
     e.target.innerHTML = 'Dark mode'
   } else {
     html.classList.add('dark');
@@ -45,17 +45,29 @@ function setTime()  {
   const time = new Date();
   const month = time.getMonth()
   const day = time.getDay()
+  const date = time.getDate()
   const hours = time.getHours()
-  const hoursForClock = hours % 12
+  const hoursForClock = hours >= 13 ? hours % 12 : hours;
   const minutes = time.getMinutes()
   const seconds = time.getSeconds()
+  const ampm = hours >= 12 ? 'PM' : 'AM'
 
-  hourEl.style.transform = `translate(-50%, -100%)  rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`
+  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`
+ minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`;
+
+    timeEl.innerHTML =  `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
+      dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
 }
 
+
+
 const scale = (num, in_min, in_max, out_min, out_max) => {
-  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out-min;
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 }
 
 setTime()
+
+setInterval(setTime, 1000)
+
 
