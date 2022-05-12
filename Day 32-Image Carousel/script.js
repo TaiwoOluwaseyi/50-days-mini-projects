08b4447261e8538c1499922c5893d84507a3b376
Mabel Taiwo <seyitaiwo21@gmail.com>
2022-05-12 16:18:51 +0100

    another commit

--- Day 32-Image Carousel/script.js
+++ Day 32-Image Carousel/script.js
@@ -1,8 +1,9 @@
-const imgs = document.getElementById('.imgs');
+const imgs = document.getElementById('imgs');
 const leftBtn = document.getElementById('left');
 const rightBtn = document.getElementById('right');
 
 const img = document.querySelectorAll('#imgs img')
+
 
 let idx = 0
 
@@ -10,7 +11,6 @@ let interval = setInterval(run, 2000)
 
 function run() {
   idx++
-
   changeImage()
 }
 
@@ -21,5 +21,28 @@ function changeImage() {
     idx = img.length - 1
   }
 
-  
+  imgs.style.transform = `translateX(${-idx * 500}px)`
 }
+
+function resetInterval(){
+  clearInterval(interval)
+  interval = setInterval(run, 2000)
+}
+
+rightBtn.addEventListener('click', () => {
+  idx++
+  
+  changeImage()
+  resetInterval()
+})
+
+leftBtn.addEventListener("click", () => {
+  idx--
+
+  changeImage();
+  resetInterval();
+});
+
+
+
+
