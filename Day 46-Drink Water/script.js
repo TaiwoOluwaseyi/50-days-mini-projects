e0f55a29e10e646e7bfe422a512d6bd87e6ab9f3
Mabel Taiwo <seyitaiwo21@gmail.com>
2022-06-13 18:35:06 +0100

    another commit

--- Day 46-Drink Water/script.js
+++ Day 46-Drink Water/script.js
@@ -1,7 +1,9 @@
 const smallCups = document.querySelectorAll(".cup-small");
-const liters = document.getElementById("liters");
+const listers = document.getElementById("liters");
 const percentage = document.getElementById("percentage");
 const remained = document.getElementById("remained");
+
+updateBigCup()
 
 smallCups.forEach((cup, idx) => {
   cup.addEventListener("click", () => highlightCups(idx));
@@ -20,5 +22,31 @@ function highlightCups(idx) {
     } else {
       cup.classList.remove("full");
     }
-  });
+  })
+
+  updateBigCup()
+
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll('.cup-small.full').length
+
+  const totalCups = smallCups.length
+
+  if (fullCups === 0) {
+    percentage.style.visibility = 'hidden'
+    percentage.style.height  = 0
+  } else {
+    percentage.style.visibility = 'visible'
+    percentage.style.height = `${fullCups / totalCups * 330}px`
+    percentage.innerText = `${fullCups / totalCups * 100}%`
+  }
+
+  if(fullCups === totalCups) {
+   remained.style.visibility = 'hidden'
+   remained.style.height = 0
+  } else {
+    remained.style.visibility = 'visible'
+    listers.innerText = `${2 - (250 * fullCups / 1000)}L`
+  }
 }
